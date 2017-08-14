1a8f8f29a515505ea511554a308b8e62d98bdc27
Rafael Cossovan <navossoc@gmail.com>
2017-08-14 03:14:04 -0300

    PRIME1 - Prime Generator

---
+++ PRIME1/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+import "math"
+
+func main() {
+	var t int
+	var m, n int
+
+	fmt.Scanln(&t)
+	for ; t > 0; t-- {
+		fmt.Scanln(&m, &n)
+		for ; m <= n; m++ {
+			if isPrime(m) {
+				fmt.Println(m)
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func isPrime(n int) bool {
+	if n < 2 {
+		return false
+	}
+
+	// Trial division
+	r := int(math.Sqrt(float64(n)))
+
+	for i := 2; i <= r; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+
+	return true
+}
